2a3dcf8f8332274effd8d89f6babe72a6073eaed
Virendra Patel <virupatel9264@gmail.com>
2024-12-02 23:35:51 +0530

    Random quote generator

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -13,5 +13,5 @@ async function getQuote(url) {
 }
 
 function tweet(){
-    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ----by" + author.innerHTML,"Tweet Window", "width=600, height=300")
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ----by" + author.innerHTML,"Tweet Window", "width=600, height=300");
 }
