6b01f267344ac40d353002a413bb9a4adbb6e553
Virendra Patel <virupatel9264@gmail.com>
2024-12-06 22:41:10 +0530

    Toast notification

---
+++ Toast_notification/script.js
@@ -0,0 +1,23 @@
+const toastBox = document.getElementById('toast-box')
+let successMsg = '<i class="fa-sharp fa-solid fa-circle-check"></i> Successfully submitted'
+let errorMsg = '<i class="fa-sharp fa-solid fa-circle-xmark"></i> Please fix the error!'
+let invalidMsg = '<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Invalid input, check again'
+
+function showToast(msg){
+    let toast = document.createElement("div")
+    toast.classList.add('toast')
+    toast.innerHTML = msg
+    toastBox.appendChild(toast)
+
+    if(msg.includes('error')){
+        toast.classList.add('error')
+    }
+
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid')
+    }
+
+    setTimeout(() => {
+        toast.remove()
+    }, 3000)
+}
