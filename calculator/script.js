5a594546ff61e96c41ec0acf388cba83ddc4f0b5
Virendra Patel <virupatel9264@gmail.com>
2024-12-09 12:04:13 +0530

    delete script.js

--- calculator/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('btn');
-button.addEventListener('click', () => {
-  button.classList.toggle('active');
-  setTimeout(() => {
-    button.classList.remove('active');
-  }, 100);
-});
